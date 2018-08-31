714929fd4d3b2b4e63c11909b1757b53072387c6
Maciej Misiak <misiak.mac@gmail.com>
2018-08-31 11:06:44 +0200

    fix cors

--- src/components/MainBox/index.jsx
+++ src/components/MainBox/index.jsx
@@ -47,6 +47,7 @@ class MainBox extends Component {
                 fetch(url, {
                     method: 'POST',
                     headers: {
+                        'Access-Control-Allow-Origin': 'https://ancient-badlands-76161.herokuapp.com/' , 
                         'Content-type': 'application/json',
                     },
                     body: JSON.stringify(data)
