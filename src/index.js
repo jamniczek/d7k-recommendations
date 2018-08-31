e910adb3be7b53e56086c53569da9fc1b172e79d
Maciej Misiak <misiak.mac@gmail.com>
2018-08-31 10:40:35 +0200

    add fetch for booting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App/index';
 import registerServiceWorker from './registerServiceWorker';
 
-
+fetch('https://secret-fortress-42712.herokuapp.com/recognize')
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
